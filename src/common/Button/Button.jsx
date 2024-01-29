19574e8ebbe29eea7b6fe3675d63fbab4ec9f09b
Çağla Gezgen <cagla_gezgen@epam.com>
2024-01-29 22:00:05 +0300

    feat(module-task2): fix button

--- src/common/Button/Button.jsx
+++ src/common/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ buttonText, onClick, icon }) => (
 	<button
 		onClick={onClick}
 		className='w-36
-  border border-purple-700 h-10 rounded-md hover:bg-purple-700 hover:text-white transition-all ease-linear duration-75'
+  border border-blue-700 h-10 rounded-md hover:bg-blue-700 hover:text-white transition-all ease-linear duration-75'
 	>
 		{icon && <img src={icon} className='w-10 mr-5' alt='' />}
 		{buttonText}
@@ -16,4 +16,5 @@ export default Button;
 Button.propTypes = {
 	buttonText: PropTypes.string.isRequired,
 	onClick: PropTypes.func,
+	icon: PropTypes.string,
 };
