fc094eb12124e66553b82ba77347cec9d0abb14b
Çağla Gezgen <cagla_gezgen@epam.com>
2024-02-23 13:51:31 +0300

    feat(task3):  fix typo

--- src/store/services.js
+++ src/store/services.js
@@ -1,9 +1,9 @@
 class APIService {
 	getCourses() {
-		return fetch(`${process.env.REACT_APP_API_UR}/courses/all`);
+		return fetch(`${process.env.REACT_APP_API_URL}/courses/all`);
 	}
 	getAuthors() {
-		return fetch(`${process.env.REACT_APP_API_UR}/authors/all`);
+		return fetch(`${process.env.REACT_APP_API_URL}/authors/all`);
 	}
 }
 
